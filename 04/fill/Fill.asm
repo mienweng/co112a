9a40df69dacd69b954da167fd21012bf68300086
mienweng <sb93129@gmail.com>
2024-01-12 01:52:58 +0800

    .

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,54 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(START)
+@SCREEN
+D=A
+@R0
+M=D
+
+@24576
+D=A
+@R1
+M=D
+
+@temp
+M=-1
+
+(LOOP)
+@24576
+D=M
+@SWITCH
+D;JEQ
+
+(CONT)
+@temp
+D=M
+@R0
+A=M
+M=D
+
+@R0
+M = M+1
+D=M
+@R1
+D = D-M
+@START
+D;JEQ
+
+
+@LOOP
+0;JMP
+
+(SWITCH)
+@temp
+M=0
+@CONT
+0;JMP
+
+(END)
+@START
+0;JMP
+
+
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,52 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//Take value in R0 and store it in a variable caled value1
+@R0
+D=M
+@value1
+M=D
+
+//Take value in R1 and store it in a variable caled value2
+@R1
+D=M
+@value2
+M=D
+
+//SET R2 to 0
+@R2 
+M=0
+
+// IF value2 is 0 END THE PROGRAM
+@value2
+D=M
+@END
+D; JEQ
+
+// Loop. Add value1 to r2 as long as value2 is above 0
+(LOOP)
+
+//Take value in R2, and value1, then add them and store in R2
+@R2
+D=M
+@value1
+D = D + M
+@R2
+M=D
+
+//Reduce value2 by 1
+@value2
+M=M-1
+D=M
+
+//Loop as long as value2 is > 0
+@LOOP
+D; JGT
+
+//END THE PROGRAM
+(END)
+@END
+0; JMP
+
+
